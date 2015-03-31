d00220425e436ea1ac70f795f31d570706fb86ed
Rastislav Lamos <lamos.rasto@gmail.com>
2015-03-31 17:12:00 +0200

    Add Theme Wrapper for better DRY

---
+++ web/app/themes/rentiflat-theme/src/helper-functions.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @ Lamosty.com 2015
+ */
+
+namespace Lamosty\RentiFlat\Helper_Functions;
+
+/**
+ * Theme wrapper
+ *
+ * @link https://roots.io/sage/docs/theme-wrapper/
+ * @link http://scribu.net/wordpress/theme-wrappers.html
+ */
+
+class Theme_Wrapper {
+	// Stores the full path to the main template file
+	public static $main_template;
+
+	// Basename of template file
+	public $slug;
+
+	// Array of templates
+	public $templates;
+
+	// Stores the base name of the template file; e.g. 'page' for 'page.php' etc.
+	public static $base;
+
+	public function __construct( $template = 'base.php' ) {
+		$this->slug      = basename( $template, '.php' );
+		$this->templates = [ $template ];
+		if ( self::$base ) {
+			$str = substr( $template, 0, - 4 );
+			array_unshift( $this->templates, sprintf( $str . '-%s.php', self::$base ) );
+		}
+	}
+
+	public function __toString() {
+		$this->templates = apply_filters( 'sage/wrap_' . $this->slug, $this->templates );
+
+		return locate_template( $this->templates );
+	}
+
+	public static function wrap( $main ) {
+		// Check for other filters returning null
+		if ( ! is_string( $main ) ) {
+			return $main;
+		}
+
+		self::$main_template = $main;
+		self::$base          = basename( self::$main_template, '.php' );
+
+		if ( self::$base === 'index' ) {
+			self::$base = false;
+		}
+
+		return new Theme_Wrapper();
+	}
+}
+
+function template_path() {
+	return Theme_Wrapper::$main_template;
+}
