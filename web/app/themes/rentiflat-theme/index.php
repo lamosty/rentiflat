053729b99bfe711672a3fc37dc31dbebc4f0d6ad
Rastislav Lamos <lamos.rasto@gmail.com>
2015-04-09 17:29:36 +0200

    Add basic landing page structure

--- web/app/themes/rentiflat-theme/index.php
+++ web/app/themes/rentiflat-theme/index.php
@@ -1,6 +1,173 @@
 <?php
 /**
  * @ Lamosty.com 2015
+ *
+ * Landing Page
  */
 
-// home page
+
+?>
+
+<section id="landing-hero">
+
+	<?php get_template_part( 'templates/partials/site-nav.php' ); ?>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="headline">
+					<h1>Helping People With Their Housing Needs</h1>
+
+					<p>Web app helping tenants and real estate owners match each other comfortably and
+						inexpensively.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</section>
+
+<section id="features-overview">
+	<div class="features-overview">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 heading center">
+					<h1>Some super useful text</h1>
+
+					<p class="subheadline">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+						sed diam nonumy eirmod tempor invidunt ut labore et dolore
+					</p>
+				</div>
+				<div class="col-md-12">
+					<ul class="main-features">
+						<li>
+							<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+
+							<h3>Feature #1</h3>
+
+							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+								invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+							</p>
+						</li>
+						<li>
+							<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+
+							<h3>Feature #2</h3>
+
+							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+								invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+							</p>
+						</li>
+						<li>
+							<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+
+							<h3>Feature #3</h3>
+
+							<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+								invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+							</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</section>
+
+<section id="features">
+	<article id="feature">
+		<div class="overlay"></div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-sm-push-6 col-md-6 col-md-push-6">
+					<div class="heading">
+						<h2>Some features</h2>
+
+						<p class="subheadline">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+							eirmod tempor invidunt ut abore et dolore magna</p>
+					</div>
+
+					<div class="feature-block">
+						<span class="glyphicon glyphicon-globe"></span>
+
+						<h3>Wordlwide</h3>
+
+						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempo</p>
+					</div>
+					<div class="feature-block">
+						<span class="glyphicon glyphicon-globe"></span>
+
+						<h3>Wordlwide</h3>
+
+						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempo</p>
+					</div>
+				</div>
+				<div class="col-sm-6 col-sm-pull-6 col-md-6 col-md-pull-6">
+					<img class="img-responsive">
+				</div>
+			</div>
+	</article>
+	<article id="feature">
+		<div class="overlay"></div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-md-6">
+					<div class="heading">
+						<h2>Some features</h2>
+
+						<p class="subheadline">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+							eirmod tempor invidunt ut abore et dolore magna</p>
+					</div>
+
+					<div class="feature-block">
+						<span class="glyphicon glyphicon-globe"></span>
+
+						<h3>Wordlwide</h3>
+
+						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempo</p>
+					</div>
+					<div class="feature-block">
+						<span class="glyphicon glyphicon-globe"></span>
+
+						<h3>Wordlwide</h3>
+
+						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempo</p>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-6">
+					<img class="img-responsive">
+				</div>
+			</div>
+	</article>
+</section>
+
+<section id="latest-flats">
+	<div class="latest-flats">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+					<div class="heading center">
+						<h1>Latest Flats</h1>
+
+						<p class="subheadline">
+							Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+							invidunt ut abore et dolore magna
+						</p>
+					</div>
+				</div>
+				<div class="col-md-12">
+					flats gallery
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 center">
+					Browse all flats
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
