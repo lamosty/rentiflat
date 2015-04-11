cd49a8c9cf56c28050097f841176aed68305b785
Rastislav Lamos <lamos.rasto@gmail.com>
2015-04-11 18:27:05 +0200

    Update dependencies

--- config/environments/development.php
+++ config/environments/development.php
@@ -11,3 +11,5 @@ define('WP_SITEURL', getenv('WP_SITEURL'));
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
+
+define('WP_DEBUG_LOG', getenv('WP_DEBUG_LOG'));
