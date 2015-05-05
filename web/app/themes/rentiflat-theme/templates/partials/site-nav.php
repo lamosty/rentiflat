15215c85576b9693d035ba144569f943682706ca
Rastislav Lamos <lamos.rasto@gmail.com>
2015-05-05 22:35:37 +0200

    Add link to logo

--- web/app/themes/rentiflat-theme/templates/partials/site-nav.php
+++ web/app/themes/rentiflat-theme/templates/partials/site-nav.php
@@ -17,14 +17,11 @@ use Lamosty\RentiFlat\Utils\Template_Helper;
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<div class="navbar-brand">RentiFlat</div>
+				<a href="<?= home_url( '/' ); ?>" class="navbar-brand">RentiFlat</a>
 			</div>
 
 			<div class="navbar-collapse collapse navbar-responsive collapse">
 				<ul class="nav navbar-nav">
-					<li class="<?= Template_Helper::nav_set_active( '/' ); ?>">
-						<a href="<?= home_url( '/' ); ?>">Home</a>
-					</li>
 					<li class="<?= Template_Helper::nav_set_active( '/find-flats/' ); ?>">
 						<a href="<?= home_url( '/find-flats/' ); ?>">Find Flats</a>
 					</li>
